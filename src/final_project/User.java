329361e3799cfe913c200ddf624073dd72bc55d7
Andrew Dufour <dufour.andrew12@gmail.com>
2018-04-24 16:14:17 -0500

    Login/Logout as well as simple test data for DB

---
+++ src/final_project/User.java
@@ -0,0 +1,82 @@
+package final_project;
+import java.sql.*;
+import final_project.DBBean;
+
+public class User {
+	
+	private String eNumber;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private boolean loggedIn;
+	private int personType;
+
+	
+	public boolean Login() {
+		boolean stat = false;
+		try {
+			System.out.println(eNumber);
+			System.out.println(password);
+			DBBean dbbean = new DBBean();
+			dbbean.InitConnection();
+			PreparedStatement ps = dbbean.getConnection().prepareStatement("select * from person where eNumber = ? and password = ?");
+			ps.setString(1, eNumber);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			stat = rs.next();
+			if(stat) {
+				firstName = rs.getString("FirstName");
+				lastName = rs.getString("LastName");
+				personType = rs.getInt("isStudent");
+				if(isStudent()) {
+					
+				}
+				loggedIn = true;
+				return true;
+			}
+		}catch(Exception e) {
+			System.out.println("Could not make a connection or invalid query");
+			return false;
+		}
+		return false;
+	}
+	
+	public void setENumber(String eNum) {
+		eNumber = eNum;
+	}
+	
+	public String getENumber() {
+		return eNumber;
+	}
+	
+	public void setPassword(String pass) {
+		password = pass;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setName(String first, String last) {
+		firstName = first;
+		lastName = last;
+	}
+	public String getName() {
+		return firstName + " " + lastName;
+	}
+	
+	public boolean isStudent() {
+		if(personType == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+	public void setLoggedIn(boolean status) {
+		loggedIn = status;
+	}
+}
---
+++ src/final_project/simpleTestData.sql
@@ -0,0 +1,3 @@
+INSERT INTO `advising_app`.`person` (`idPerson`, `eNumber`, `password`, `isStudent`, `FirstName`, `LastName`) VALUES ('0', 'e0000000', 'test', '0', 'Andrew', 'D\n');
+
+INSERT INTO `advising_app`.`person` (`idPerson`, `eNumber`, `password`, `isStudent`, `FirstName`, `LastName`) VALUES ('1', 'e0123456', 'test', '1', 'Matt', 'W');

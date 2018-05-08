05881ff69bf2ceaed85a8dd67f633cb6b69647d9
Matthew-Weiss <matthew.weiss@365.elmhurst.edu>
2018-05-08 10:42:59 -0500

    3rd Generation (3rd) SQL Tables, new Advisor.java

--- src/final_project/Advisor.java
+++ src/final_project/Advisor.java
@@ -1,5 +1,71 @@
 package final_project;
+import java.sql.*;
+import final_project.DBBean;
+import java.util.*;
 
 public class Advisor {
-
+	private DBBean dbbean;
+	private String eNumber;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private boolean loggedIn;
+	private Map<String, String> studentinfo;
+	private int personType;
+	
+	Advisor(DBBean db, String eN, String fN, String lN) {
+		dbbean = db;
+		eNumber = eN;
+		firstName = fN;
+		lastName = lN;
+		studentinfo = new HashMap<String, String>();
+		// can check if null, then throw error
+	}
+	
+	// we need to get each student's eNumber, FirstName, LastName, Major, (and even scheduler for later use). 
+	boolean getstudents() {
+		try {
+			boolean stat;
+			PreparedStatement ps = dbbean.getConnection().prepareStatement("select adviseeList from advisor where eNumber = ?");
+			ps.setString(1, eNumber);
+			ResultSet rs = ps.executeQuery();
+			stat = rs.next(); 
+			// how can you ensure that only one was selected? if two selected then error...
+			if(stat) {
+				String adviseeList = rs.getString("adviseeList");
+				String[] studentENumber = adviseeList.split(",");
+				
+				
+				for(String studentstr : studentENumber) {
+					boolean stat2;
+					PreparedStatement ps2 = dbbean.getConnection().prepareStatement(
+							"select firstName, lastName, major.fullname from person, student join major on student.idMajor = major.idMajor where person.eNumber = ? && student.eNumberStudent = person.eNumber;"
+							);
+					ps2.setString(1, studentstr);
+					ResultSet rs2 = ps2.executeQuery();
+					stat2 = rs2.next(); 
+					if(stat2) {
+						String temp = rs2.toString();
+						System.out.println(temp);
+						studentinfo.put(studentstr, temp); 
+					}
+					else {
+						System.out.println("Error!");
+					}
+					rs2.close();
+				}
+				rs.close();
+				return true;
+			}
+			else // advisor has no students
+			{
+				rs.close();
+				return true;
+			}
+		}catch(Exception e) {
+			System.out.println("Invalid query");
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
